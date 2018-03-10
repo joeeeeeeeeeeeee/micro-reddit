87ecb50777e814265c0d9c546fa6fc2a0a698f06
Joe Weston <joeweston6@gmail.com>
2018-03-10 21:00:18 +0000

    branch complete, users correctly linked to posts

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,7 +2,10 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
   def setup
-    @post = Post.new(title: "Test Title", content: "random text")
+    @user = User.create(username: "Joe Bloggs", email: "joebloggs@example.com")
+
+    @post = Post.create(title: "Test Title", content: "random text",
+                     user_id: @user.id)
   end
 
   test "should be vailid" do
@@ -14,5 +17,10 @@ class PostTest < ActiveSupport::TestCase
     assert @post.title != "another string"
     assert @post.content == "random text"
     assert @post.content != "other text"
+  end
+
+  test "definately linked to user" do
+    assert User.find(@post.user_id).username == "Joe Bloggs"
+    assert User.find(@post.user_id).username != "Joe Cloggs"
   end
 end
